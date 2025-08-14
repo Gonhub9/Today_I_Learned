0b37c55c0e0e45800d79a30cd1d96d8b14b1f89b
Gonhub9 <ho_gok@naver.com>
2025-08-14 09:20:16 +0900

    feat: 토큰, 권한 클래스 작성

---
+++ src/main/java/gon/til/global/jwt/JwtTokenProvider.java
@@ -0,0 +1,53 @@
+package gon.til.global.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public String createToken(String username) {
+        Date now = new Date();
+        // 1시간
+        long accessTokenValidityInMilliseconds = 3600000;
+        Date validity = new Date(now.getTime() + accessTokenValidityInMilliseconds);
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(now)
+                .expiration(validity)
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
